81bc42861acbc08715875978be20588786c3cba0
mr-emeka <speak2c.emeka@gmail.com>
2025-08-24 12:12:09 -0700

    chore: set one

---
+++ index.ts
@@ -0,0 +1,8 @@
+// index.js
+export function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function reverse(str: string) {
+  return str.split("").reverse().join("");
+}
